1353509fba7deea942b90e144a28d3e5ce290448
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-11-21 12:30:07 -0300

    login, falta css

--- proyectointegrador/src/screens/Login.js
+++ proyectointegrador/src/screens/Login.js
@@ -13,5 +13,58 @@ class Login extends Component {
       };
     }
 
+    componentDidMount(){
+      auth.onAuthStateChanged(user => console.log ('El usuario es:', JSON.stringify(user,null,4)))
+    }
+
+    handleSubmit(){
+      auth
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then((response) => this.setState({logued: true}))
+        .then(()=> this.props.navigation.navigate("HomeMenu"))
+        .catch((error) => this.setState({error: "Fallo el login"}));
+    } 
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.heading}>Ingresar</Text>
+          <TextInput
+            keyboardType="email-address"
+            placeholder="Ingrese su email"
+            onChangeText={(text) => this.setState({ email: text })}
+            value={this.state.email}
+          />
+          <TextInput
+            placeholder="Ingrese su contrasena"
+            secureTextEntry={true}
+            onChangeText={(text) => this.setState({ password: text })}
+            value={this.state.password}
+          />
+          <TouchableOpacity onPress={() => this.handleSubmit() }  style={[styles.button, styles.buttonSecondary]}>
+            <Text>Acceder</Text>
+          </TouchableOpacity>
+          <Text>Navegación cruzada a Register: </Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Register")}
+            style={styles.button}
+          >
+            <Text>No tengo cuenta</Text>
+          </TouchableOpacity>
+          <Text>
+            Navegación cruzada a ingresar a la app. Este paso se hará
+            automaticamente cuando veamos la funcionalidad de loguin{" "}
+          </Text>      
+        </View>
+      );
+    }
+
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+})
 export default Login;
